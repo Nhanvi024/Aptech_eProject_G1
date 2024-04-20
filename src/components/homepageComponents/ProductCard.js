c581d033b280bbbe10ad24022607fe0c513a9ba7
unknown <dnthoang1295@gmail.com>
2024-04-20 23:19:32 +0700

    test homepage card

---
+++ src/components/homepageComponents/ProductCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ProductCard.css'
+function ProductCard(props) {
+    let { item } = props
+    return (
+        <div className='text-center mx-auto col homepage-productCard'>
+            <div>
+                <div className='homepage-productCard-imgContainer'>
+                    <img className='homepage-productCard-img w-100' src={item.image.mainImage}></img>
+                </div>
+                <div>
+                    <div className='homepage-productCard-name'>{item.name}</div>
+                    <div className='homepage-productCard-price'>${item.price}</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProductCard;
