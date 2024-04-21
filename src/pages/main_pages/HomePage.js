17834b9f78894b0210c2c17e0002d7833d911cb6
unknown <dnthoang1295@gmail.com>
2024-04-21 15:06:42 +0700

    test data card

--- src/pages/main_pages/HomePage.js
+++ src/pages/main_pages/HomePage.js
@@ -28,13 +28,13 @@ function HomePage(props) {
     let itemPerPage = windowWidth >= 1200 ? 8 : windowWidth >= 992 ? 6 : windowWidth >= 768 ? 4 : windowWidth >= 576 ? 4 : 4;
 
     let section1MaxPage = Math.floor(+Object.keys(products.noteBook || {}).length / itemPerPage);
-    console.log("item per page1: ", itemPerPage);
-    console.log("max page 1:", section1MaxPage);
-    console.log("product notebook length: ", Object.keys(products.noteBook || {}).length);
+    // console.log("item per page1: ", itemPerPage);
+    // console.log("max page 1:", section1MaxPage);
+    // console.log("product notebook length: ", Object.keys(products.noteBook || {}).length);
 
     // console.log("test item per page: ",itemPerPage);
     let [section1Page, setSection1Page] = useState(0)
-    console.log("section1Page: ", section1Page);
+    // console.log("section1Page: ", section1Page);
     function handleSection1PageNext(params) {
         if (section1Page < section1MaxPage) {
             setSection1Page(pre => pre + 1)
@@ -42,7 +42,6 @@ function HomePage(props) {
             setSection1Page(0)
         }
     }
-    console.log("section1 Page: ", section1Page);
     function handleSection1PagePrev(params) {
         if (section1Page > 0) {
             setSection1Page(pre => pre - 1)
@@ -50,8 +49,34 @@ function HomePage(props) {
             setSection1Page(section1MaxPage)
         }
     }
-    let a = (section1Page + 1) * itemPerPage;
-    console.log("test a:", a);
+
+    // 
+    let category1List = ['all'];
+    {
+        products.length !== 0 && products.noteBook.map((item, index) => {
+            if (!category1List.includes(item.type)) {
+                category1List = [...category1List, item.type];
+            }
+        })
+    };
+    // console.log("test category1List:", category1List);
+    //
+
+    //
+    const [notebookList, setNotebookList] = useState([]);
+    const [notebookTypeFilter, setNotebookTypeFilter] = useState('all')
+    console.log(notebookTypeFilter);
+    useEffect(() => {
+        // let result
+        if (notebookTypeFilter == 'all') {
+            setNotebookList(products.noteBook)
+        } else {
+           let result = products.noteBook.filter(item => item.type == notebookTypeFilter)
+            // console.log("test item list:",notebookList);
+            setNotebookList(result)
+        }
+    },[notebookTypeFilter])
+    console.log("test notebooklist:", notebookList);
     return (
         <div className='homePageContainer container-fluid p-0'>
             <section className="section-homepage-carousel">
@@ -113,31 +138,30 @@ function HomePage(props) {
             </section>
             <section className='section-calendar container-md p-0 mt-3'>
                 <div className='row w-100 g-0 flex-row-reverse align-items-center'>
-                    <div className='col-sm-8 bg-info px-3'>
+                    <div className='col-sm-8 px-3'>g
                         <div className='row '>
-                            <div className='row m-auto mb-2 bg-danger'>
+                            <div className='row m-auto mb-2'>
                                 <div className='col-8'>
                                     <ul className="nav nav-tabs">
-                                        <li className="nav-item">
-                                            <a className="nav-link active" href="#">Active</a>
-                                        </li>
-                                        <li className="nav-item">
-                                            <a className="nav-link" href="#">Link</a>
-                                        </li>
-                                        <li className="nav-item">
-                                            <a className="nav-link" href="#">Link</a>
-                                        </li>
-                                        <li className="nav-item">
-                                            <a className="nav-link" href="#">Disabled</a>
-                                        </li>
+                                        {products.length !== 0 && category1List.map((item, index) => {
+                                            return (
+                                                <li className="nav-item" key={index}>
+                                                    <a className="nav-link" onClick={() => setNotebookTypeFilter(item)} >{item}</a>
+                                                </li>
+                                            )
+                                        })}
                                     </ul>
                                 </div>
                                 <div className='col-4 text-end'>
-                                    <button className="btn btn-primary btn-lg me-1" onClick={handleSection1PagePrev}><p className='homepage-nextPrevButton'>&#8249;</p></button>
-                                    <button className="btn btn-primary btn-lg ms-1" onClick={handleSection1PageNext}><p className='homepage-nextPrevButton'>&#8250;</p></button>
+                                    <button className="btn btn-primary btn-lg me-1" onClick={handleSection1PagePrev}>
+                                        <p className='homepage-nextPrevButton'>&#8249;</p>
+                                    </button>
+                                    <button className="btn btn-primary btn-lg ms-1" onClick={handleSection1PageNext}>
+                                        <p className='homepage-nextPrevButton'>&#8250;</p>
+                                    </button>
                                 </div>
                             </div>
-                            <div className='row m-auto bg-warning align-items-center' style={{height:"480px"}}>
+                            <div className='row m-auto align-items-center' style={{ height: "480px" }}>
                                 {products.length === 0 &&
                                     <div className="d-flex justify-content-center">
                                         <div className="spinner-border text-danger" style={{ width: "5rem", height: "5rem" }} role="status">
@@ -149,17 +173,16 @@ function HomePage(props) {
                                         return (
                                             <ProductCard key={index} item={item} />
                                         )
-
                                     }
                                 })}
                             </div>
                         </div>
                     </div>
-                    <div className='col-sm-4 bg-primary homepage-notebookCarousel-container p-3'>
-                        <div id="homepage-notebookCarousel" className="carousel slide text-center" data-bs-ride="carousel">
+                    <div className='col-sm-4 homepage-notebookCarousel-container p-3 d-flex' style={{ height: "480px" }}>
+                        <div id="homepage-notebookCarousel" className="carousel slide text-center m-auto w-100" data-bs-ride="carousel">
 
                             {/* <!-- Wrapper for slides --> */}
-                            <div className="carousel-inner">
+                            <div className="carousel-inner d-flex">
                                 {products.length === 0 &&
                                     <div className="d-flex justify-content-center">
                                         <div className="spinner-border text-danger" style={{ width: "5rem", height: "5rem" }} role="status">
@@ -170,10 +193,11 @@ function HomePage(props) {
                                     let classCarousel = index === 0 ? "carousel-item active" : "carousel-item"
                                     return (
                                         <div key={index} className={classCarousel} data-bs-interval="3000">
-                                            <img
-                                                src={item.image.mainImage}
-                                                alt="Ribbon Collection"
-                                                className='d-block w-100' />
+                                            <div className='homepage-productCard-carouselImgContainer'>
+                                                <img
+                                                    src={item.image.mainImage}
+                                                    className='d-flex w-100 homepage-productCard-carouselImg' />
+                                            </div>
                                             <div>
                                                 <div className='homepage-productCard-name'>{item.name}</div>
                                                 <div className='homepage-productCard-price'>${item.price}</div>
@@ -184,11 +208,11 @@ function HomePage(props) {
                             </div>
 
                             {/* <!-- Left and right controls --> */}
-                            <button className="carousel-control-prev" data-bs-target="#notebookCarousel" data-bs-slide="prev">
-                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <button className="carousel-control-prev" data-bs-target="#homepage-notebookCarousel" data-bs-slide="prev">
+                                <span className="" aria-hidden="true"></span>
                             </button>
-                            <button className="carousel-control-next" data-bs-target="#notebookCarousel" data-bs-slide="next">
-                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <button className="carousel-control-next" data-bs-target="#homepage-notebookCarousel" data-bs-slide="next">
+                                <span className="" aria-hidden="true"></span>
                             </button>
                         </div>
                     </div>
