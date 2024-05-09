2bed93ebbab905b3034bae487b03eb7f1dcd9a64
Minh Phú Nguyễn <nguyenminhphu250797@gmail.com>
2024-05-09 15:31:45 +0700

    fix animation

--- src/pages/extra_pages/SearchPage.js
+++ src/pages/extra_pages/SearchPage.js
@@ -31,7 +31,7 @@ function SearchPage(props) {
             <motion.div initial={{ opacity: 0 }}
 
                 animate={{ opacity: 1, transition: { duration: 1 } }}
-                exit={{ opacity: 0, transition: { duration: 0.1 } }}
+                exit={{ opacity: 0, transition: { duration: 0} }}
             >
                 <img className='imageBanner'
                     src='https://carleton.ca/dighum/wp-content/uploads/Calendar-Banner.jpg'></img>
