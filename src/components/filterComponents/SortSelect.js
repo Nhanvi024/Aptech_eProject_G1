db93e9eb02a50b7e38e26effc5b8947d2246fe95
Nhan Vi <nhanvi9395@gmail.com>
2024-05-13 20:25:31 +0700

    prepare to submit

--- src/components/filterComponents/SortSelect.js
+++ src/components/filterComponents/SortSelect.js
@@ -1,17 +1,23 @@
 import './SortSelect.css'
 
 import React from 'react';
+import $ from 'jquery'
 
 function SortSelect(props) {
 const {select, setSelect, selectArray} = props
     return (
         <div className='sortSelect-container'> 
             <p className='sortSelect-label'>Sort by: </p>
-            <p className='sortSelect-select'>{select.name}<span className="sortSelect-icon" /></p>
-            <div className='sortSelect-list'>
+            <p onClick={()=>$('#idsortSelect-list').slideToggle()} className='sortSelect-select'>{select.name}<span className="sortSelect-icon" /></p>
+            <div className='sortSelect-list' id='idsortSelect-list'>
                 {selectArray.map((item, index) => {
                     return (
-                        <p key={index} className='sortSelect-option' onClick={() => { setSelect(item) }} value={item.value}>{item.name}</p>
+                        <p key={index} className='sortSelect-option' 
+                        onClick={() => { 
+                            setSelect(item) 
+                            $('#idsortSelect-list').slideUp()
+                        }} 
+                        value={item.value}>{item.name}</p>
                     )
                 })}
             </div>
