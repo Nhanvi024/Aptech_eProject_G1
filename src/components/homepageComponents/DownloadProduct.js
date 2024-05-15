ab7eb3ac52f7f5fa81e5ea4cf3435c4ec8a74f77
unknown <dnthoang1295@gmail.com>
2024-05-15 11:05:42 +0700

    tinh chinh lai code

--- src/components/homepageComponents/DownloadProduct.js
+++ src/components/homepageComponents/DownloadProduct.js
@@ -1,22 +1,13 @@
-import React, {useContext, useEffect, useRef, useState} from 'react';
+import React, {useEffect} from 'react';
 import './DownloadProduct.css';
 import {useLocation, useNavigate} from 'react-router-dom';
 import {jsPDF} from 'jspdf';
-import html2canvas from 'html2canvas';
-import {Callbacks} from 'jquery';
-import { motion } from 'framer-motion';
+import {motion} from 'framer-motion';
 
 function DownloadProduct(props) {
-	// let {item} = props;
 	const location = useLocation();
 	const item = location.state;
 	const navigate = useNavigate();
-	console.log('-----------------');
-	console.log('test pass item:', item);
-	console.log('type of pass item:', typeof item);
-	console.log('-----------------');
-	const trang1 = useRef();
-	const trang2 = useRef();
 	function generatePDF() {
 		let pdf = new jsPDF('p', 'px', 'a4');
 		let input = document.getElementById('download-content');
@@ -35,84 +26,79 @@ function DownloadProduct(props) {
 			},
 		};
 		pdf.html(input, opt);
-
-		console.log(trang1.current);
-		console.log('input test:', input);
 	}
 	useEffect(() => {
 		generatePDF();
 		navigate(-1);
 	}, [item]);
 
-	
 	return (
-		<motion.div initial={{ opacity: 0 }}
-
-		animate={{ opacity: 0 }}
-		exit={{ opacity: 0 }}
-            >
-		<div className="downloadproduct-container">
-			<div className="content row">
-				<div className="container row" id="download-content">
-					<div className="download-text-container" style={{fontSize: 'var(--fs-600)', fontFamily: 'var(--ff-primary-font)'}}>
-						<div className="text-danger" style={{fontSize: '55px'}}>
-							{item.name}
-						</div>
-						<div className="">
-							<div className="fs-2">
-								<span className="fw-bold fs-1 text-primary">Category:</span> {item.type}
+		<motion.div initial={{opacity: 0}} animate={{opacity: 0}} exit={{opacity: 0}}>
+			<div className="downloadproduct-container">
+				<div className="content row">
+					<div className="container row" id="download-content">
+						<div
+							className="download-text-container"
+							style={{fontSize: 'var(--fs-600)', fontFamily: 'var(--ff-primary-font)'}}
+						>
+							<div className="text-danger" style={{fontSize: '55px'}}>
+								{item.name}
 							</div>
-						</div>
-						<br />
-						<div className="modal-priceTag justify-items-center ms-3 my-4 pt-1">
-							<div className="modal-priceTag-text px-5 py-2" style={{fontSize: '40px'}}>
-								${item.price}
+							<div className="">
+								<div className="fs-2">
+									<span className="fw-bold fs-1 text-primary">Category:</span> {item.type}
+								</div>
 							</div>
-						</div>
-						<br />
-						<div className="">
 							<br />
-							<div className="fw-bold fs-1 text-primary">Description</div>
+							<div className="modal-priceTag justify-items-center ms-3 my-4 pt-1">
+								<div className="modal-priceTag-text px-5 py-2" style={{fontSize: '40px'}}>
+									${item.price}
+								</div>
+							</div>
+							<br />
+							<div className="">
+								<br />
+								<div className="fw-bold fs-1 text-primary">Description</div>
+								<hr />
+								<div className="fs-2">{item.desc}</div>
+								<br />
+								<div className="fs-2">{item.descDetail}</div>
+								<br />
+							</div>
+							<div>
+								<div className="fw-bold fs-1 text-primary">Specification</div>
+								<hr />
+								<div className="fs-2">Dimensions: {item.param.size}</div>
+								<div className="fs-2">Weight: {item.param.weight}</div>
+								<div className="fs-2">Pages: {item.param.pages}</div>
+								<div className="fs-2">Material: {item.param.material}</div>
+								<br />
+							</div>
+						</div>
+						<div className="container p-5"></div>
+						<div className="download-img-container">
+							<h1 className="text-danger">Product images</h1>
 							<hr />
-							<div className="fs-2">{item.desc}</div>
-							<br />
-							<div className="fs-2">{item.descDetail}</div>
-							<br />
-						</div>
-						<div>
-							<div className="fw-bold fs-1 text-primary">Specification</div>
-							<hr />
-							<div className="fs-2">Dimensions: {item.param.size}</div>
-							<div className="fs-2">Weight: {item.param.weight}</div>
-							<div className="fs-2">Pages: {item.param.pages}</div>
-							<div className="fs-2">Material: {item.param.material}</div>
-							<br />
-						</div>
-					</div>
-					<div className="container p-5"></div>
-					<div className="download-img-container">
-						<h1 className="text-danger">Product images</h1>
-						<hr />
-						<div className="row h-50">
-							<div className="col-6 h-100">
-								<img src={item.image.mainImage} className="largeImg d-block w-100" alt="..." />
+							<div className="row h-50">
+								<div className="col-6 h-100">
+									<img src={item.image.mainImage} className="largeImg d-block w-100" alt="..." />
+								</div>
+								<div className="col-6 h-100">
+									<img src={item.image.image2} className="largeImg d-block w-100" alt="..." />
+								</div>
 							</div>
-							<div className="col-6 h-100">
-								<img src={item.image.image2} className="largeImg d-block w-100" alt="..." />
-							</div>
-						</div>
-						<div className="row h-50">
-							<div className="col-6 h-100">
-								<img src={item.image.image1} className="largeImg d-block w-100" alt="..." />
-							</div>
-							<div className="col-6 h-100">
-								<img src={item.image.image3} className="largeImg d-block w-100" alt="..." />
+							<div className="row h-50">
+								<div className="col-6 h-100">
+									<img src={item.image.image1} className="largeImg d-block w-100" alt="..." />
+								</div>
+								<div className="col-6 h-100">
+									<img src={item.image.image3} className="largeImg d-block w-100" alt="..." />
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 		</motion.div>
 	);
 }
--- src/components/homepageComponents/ProductCard.js
+++ src/components/homepageComponents/ProductCard.js
@@ -2,8 +2,7 @@ import React, {useContext, useState} from 'react';
 import './ProductCard.css';
 import {DataContext} from '../../context/DataContext';
 import Modal from 'react-bootstrap/Modal';
-import {Link, useNavigate} from 'react-router-dom';
-import DownloadProduct from './DownloadProduct';
+import {useNavigate} from 'react-router-dom';
 
 function ProductCard(props) {
 	let {item} = props;
@@ -16,14 +15,7 @@ function ProductCard(props) {
 	}
 	function handleShow(item) {
 		setShow(true);
-		// console.log('test modal item:', item);
 	}
-	// console.log('test item: ', item);
-	function handleDownloadProduct(item) {
-		navigate('/downloadproduct');
-	}
-	// console.log("test item:",item);
-	// console.log(typeof(item));
 	return (
 		<>
 			<div className="text-center mx-auto col homepage-productCard my-2">
@@ -33,14 +25,14 @@ function ProductCard(props) {
 							className="homepage-productCard-img"
 							src={item.image.mainImage}
 							alt="..."
-							onClick={() => navigate(`/detail/${item.id}`)}></img>
+							onClick={() => navigate(`/detail/${item.id}`)}
+						></img>
 						<div className="productCard-button-container">
 							{/* button add to cart */}
 							<button className="productCard-button1 fas fa-cart-plus" onClick={() => handleAddProductCart(item)}></button>
 							{/* button toggle modal */}
 							<button className="productCard-button1 fas fa-info" onClick={() => handleShow(item)}></button>
 							{/* button product detail */}
-							{/* <button className="productCard-button1 fas fa-eye" onClick={() => navigate(`/detail/${item.id}`)}></button> */}
 						</div>
 					</div>
 					<div>
@@ -49,17 +41,16 @@ function ProductCard(props) {
 					</div>
 				</div>
 			</div>
-			{/* <div style={{ position: "fixed", zIndex: "10", left: "50%", bottom: "50%" }}> */}
 			<Modal
 				show={show}
 				onHide={handleClose}
-				// backdrop="static"
 				keyboard={false}
 				animation={false}
 				centered
 				size="xl"
 				fullscreen="sm-down"
-				scrollable={true}>
+				scrollable={true}
+			>
 				<Modal.Header closeButton>
 					<Modal.Title>Product's detail</Modal.Title>
 				</Modal.Header>
@@ -67,7 +58,8 @@ function ProductCard(props) {
 					{isDataLoaded && (
 						<div
 							className="container row p-0 mx-auto p-3"
-							style={{fontSize: 'var(--fs-600)', fontFamily: 'var(--ff-primary-font)'}}>
+							style={{fontSize: 'var(--fs-600)', fontFamily: 'var(--ff-primary-font)'}}
+						>
 							<div className="col-12 p-0 col-lg-6">
 								<div id="modalCarousel" className="carousel slide" data-bs-ride="carousel">
 									<div className="carousel-inner carda">
@@ -91,7 +83,8 @@ function ProductCard(props) {
 											data-bs-slide-to="0"
 											className="thumbnail active"
 											aria-current="true"
-											aria-label="Slide 1">
+											aria-label="Slide 1"
+										>
 											<img src={item.image.mainImage} className="thumbImg d-block w-100" alt="..." />
 										</button>
 										<button
@@ -99,7 +92,8 @@ function ProductCard(props) {
 											type="button"
 											data-bs-target="#modalCarousel"
 											data-bs-slide-to="1"
-											aria-label="Slide 2">
+											aria-label="Slide 2"
+										>
 											<img src={item.image.image1} className="thumbImg d-block w-100" alt="..." />
 										</button>
 										<button
@@ -107,7 +101,8 @@ function ProductCard(props) {
 											type="button"
 											data-bs-target="#modalCarousel"
 											data-bs-slide-to="2"
-											aria-label="Slide 3">
+											aria-label="Slide 3"
+										>
 											<img src={item.image.image2} className="thumbImg d-block w-100" alt="..." />
 										</button>
 										<button
@@ -115,7 +110,8 @@ function ProductCard(props) {
 											type="button"
 											data-bs-target="#modalCarousel"
 											data-bs-slide-to="3"
-											aria-label="Slide 4">
+											aria-label="Slide 4"
+										>
 											<img src={item.image.image3} className="thumbImg d-block w-100" alt="..." />
 										</button>
 									</div>
@@ -129,7 +125,8 @@ function ProductCard(props) {
 							</div>
 							<div
 								className="col-12 p-0 col-lg-6 p-5"
-								style={{fontSize: 'var(--fs-500)', fontFamily: 'var(--ff-primary-font)'}}>
+								style={{fontSize: 'var(--fs-500)', fontFamily: 'var(--ff-primary-font)'}}
+							>
 								<h1 className="text-danger">{item.name}</h1>
 								<div className="row">
 									<div className="col">Category: {item.type}</div>
@@ -144,7 +141,8 @@ function ProductCard(props) {
 										style={{fontSize: 'var(--fs-500)'}}
 										onClick={() => {
 											handleAddProductCart(item);
-										}}>
+										}}
+									>
 										Add to cart
 									</button>
 									<button
@@ -154,14 +152,16 @@ function ProductCard(props) {
 											handleAddProductCart(item);
 											handleClose();
 											navigate('/cart');
-										}}>
+										}}
+									>
 										Buy now
 									</button>
 									<br />
 									<button
 										className="btn btn-lg mx-1 px-5 my-1 btn-danger"
 										style={{fontSize: 'var(--fs-500)'}}
-										onClick={() => navigate('/downloadproduct', {state: item})}>
+										onClick={() => navigate('/downloadproduct', {state: item})}
+									>
 										Download PDF
 									</button>
 								</div>
@@ -187,7 +187,8 @@ function ProductCard(props) {
 									<button
 										className="btn btn-lg mx-1 px-5 my-1 btn-danger"
 										style={{fontSize: 'var(--fs-500)'}}
-										onClick={() => handleClose()}>
+										onClick={() => handleClose()}
+									>
 										Close
 									</button>
 								</div>
@@ -195,14 +196,7 @@ function ProductCard(props) {
 						</div>
 					)}
 				</Modal.Body>
-				{/* <Modal.Footer>
-					<Button variant="danger" onClick={handleClose}>
-						Close
-					</Button>
-					<Button variant="primary">Understood</Button>
-				</Modal.Footer> */}
 			</Modal>
-			{/* </div> */}
 		</>
 	);
 }
