0d8ba14984ffa41699310ce3ae1a37c13b9aab5f
Nhan Vi <nhanvi9395@gmail.com>
2024-05-16 15:55:28 +0700

    prepare submit

--- src/pages/extra_pages/AllProductsPage.js
+++ src/pages/extra_pages/AllProductsPage.js
@@ -189,7 +189,7 @@ function AllProductsPage(props) {
 
     return (
         <>
-            <motion.div initial={{ opacity: 0 }}
+            <motion.div className='motionDivContainer' initial={{ opacity: 0 }}
 
                 animate={{ opacity: 1, transition: { duration: 1 } }}
                 exit={{ opacity: 0, transition: { duration: 0 } }}
