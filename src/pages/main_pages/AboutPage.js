141d5a3a77eefa4b941d8704469a4e3cd7390159
Nhan Vi <nhanvi9395@gmail.com>
2024-05-16 15:55:28 +0700

    prepare submit

--- src/pages/main_pages/AboutPage.js
+++ src/pages/main_pages/AboutPage.js
@@ -15,7 +15,7 @@ function AboutPage(props) {
 	}, [members])
 	return (
 		<>
-			<motion.div
+			<motion.div className='motionDivContainer'
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1, transition: { duration: 1 } }}
 				exit={{ opacity: 0, transition: { duration: 0 } }}>
